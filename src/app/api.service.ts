63b8e09fe1536c470a0278c09ad6fc56626bd8fb
alex.jacquot-fernandez <alex.jacquot-fernandez@etu.univ-tlse2.fr>
2019-02-22 17:35:08 +0100

    Improved front-end and added some service stuff

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Armor } from './armor';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,12 @@ export class ApiService {
   apiCharm: string = 'https://mhw-db.com/charms';
   apiDecoration: string = 'https://mhw-db.com/decorations';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
+
+  public getArmor() {}
+
+  /*return this.httpClient.get<Armor[]>(this.apiArmor,
+    { observe: 'response' }).pipe(tap(res => {
+      this.retrieve_pagination_links(res); 
+    }));*/
 }
